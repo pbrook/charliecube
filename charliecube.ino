4a15eabb5cb1982b184c16c9a8fda74f848c4582
Paul Brook <paul@nowt.org>
2014-04-30 04:10:07 +0100

    Initial checkin
    Signed-off-by: Paul Brook <paul@nowt.org>

---
+++ charliecube.ino
@@ -0,0 +1,157 @@
+/*
+  CharlieCube
+  Copyright (C) Paul Brook <paul@nowt.org>
+  Released under the terms of the GNU General Public License version 3
+
+  4x4x4 charlieplexed LED cube driver
+ */
+ 
+#include <TimerOne.h>
+
+// pin mappings are as follows:
+// Arduino pin:  3  2  0  1  4  9 10  6 A3 A2 A1 A0
+// AVR pin:	D0 D1 D2 D3 D4 B5 B6 D7 F4 F5 F6 F7
+
+#define PD_SHIFT 0
+#define PD_MASK 0x9f
+#define PB_SHIFT 0
+#define PB_MASK 0x60
+#define PF_SHIFT 4
+#define PF_MASK 0xf0
+
+#define NUM_PIXELS (4*4*4)
+
+volatile uint16_t framebuffer[12 * 16];
+
+// This table translates from logical pixel locations to the
+// anode/cathode matrix used by the framebuffer
+const uint8_t pixel_map[NUM_PIXELS] PROGMEM = {
+#define PIXEL(a, k) ((a) | ((k) << 4))
+#include "board.h"
+};
+
+static void
+tick(void)
+{
+  volatile uint16_t *p;
+  static uint8_t cathode;
+  uint8_t i;
+  uint16_t val;
+  uint16_t cathode_mask;
+
+  cathode_mask = 1u << cathode;
+  p = &framebuffer[cathode * 16];
+  /* tristate everything.  */
+  DDRD = 0;
+  DDRB = 0;
+  DDRF = 0;
+  for (i = 0; i < 16; i++) {
+      val = *(p++);
+      PORTD = (val >> PD_SHIFT) & PD_MASK;
+      PORTB = (val >> PB_SHIFT) & PB_MASK;
+      PORTF = (val >> PF_SHIFT) & PF_MASK;
+      val |= cathode_mask;
+      DDRD = (val >> PD_SHIFT) & PD_MASK;
+      DDRB = (val >> PB_SHIFT) & PB_MASK;
+      DDRF = (val >> PF_SHIFT) & PF_MASK;
+  }
+  cathode++;
+  if (cathode == 12)
+    cathode = 0;
+}
+
+static void
+set_pixel(uint8_t x, uint8_t y, uint8_t z, uint8_t val)
+{
+  uint8_t pixel_pos;
+  uint8_t pos;
+  uint16_t mask;
+  volatile uint16_t *p;
+  uint8_t i;
+
+  x &= 3;
+  y &= 3;
+  z &= 3;
+  val >>= 4;
+  pixel_pos = x + y * 8;
+  if (z & 1)
+    pixel_pos += 4;
+  if (z & 2)
+    pixel_pos += 32;
+  pos = pgm_read_byte(&pixel_map[pixel_pos]);
+  p = &framebuffer[pos & 0xf0];
+  mask = 1u << (pos & 0xf);
+  for (i = 0; i < val; i++)
+    *(p++) |= mask;
+  mask = ~mask;
+  for (; i < 16; i++)
+    *(p++) &= mask;
+}
+
+void setup()
+{
+  Timer1.initialize(1000);
+  Timer1.attachInterrupt(tick);
+}
+
+void set_xplane(uint8_t x, uint8_t val)
+{
+  uint8_t y;
+  uint8_t z;
+  for (y = 0; y < 4; y++)
+    for (z = 0; z < 4; z++)
+      set_pixel(x, y, z, val);
+}
+
+void set_yplane(uint8_t y, uint8_t val)
+{
+  uint8_t x;
+  uint8_t z;
+  for (x = 0; x < 4; x++)
+    for (z = 0; z < 4; z++)
+      set_pixel(x, y, z, val);
+}
+
+void set_zplane(uint8_t z, uint8_t val)
+{
+  uint8_t x;
+  uint8_t y;
+  for (x = 0; x < 4; x++)
+    for (y = 0; y < 4; y++)
+      set_pixel(x, y, z, val);
+}
+
+#define D 200
+// the loop routine runs over and over again forever:
+void loop()
+{
+  int8_t i;
+  //set_zplane(2, 0xff);
+  //while(1);
+  for (i = 0; i < 4; i++) {
+    set_xplane(i, 0xff);
+    delay(D);
+    set_xplane(i, 0);
+  }
+  for (i = 2; i >= 0;  i--) {
+    set_xplane(i, 0xff);
+    delay(D);
+    set_xplane(i, 0);
+  }
+  for (i = 0; i < 4; i++) {
+    set_yplane(i, 0xff);
+    delay(D);
+    set_yplane(i, 0);
+  }
+  for (i = 2; i >= 0; i--) {
+    set_yplane(i, 0xff);
+    delay(D);
+    set_yplane(i, 0);
+  }
+  for (i = 0; i < 4; i++) {
+    set_zplane(i, 0xff);
+    delay(D);
+    set_zplane(i, 0);
+  }
+}
+
